bd7c23cf7e27d16aba1a7c2fb05aa0a24da98a48
Erick Weil <erick.weil@ifro.edu.br>
2022-11-30 22:29:49 -0300

    Adicionado Cor

--- canvas/main.go
+++ canvas/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 )
@@ -17,9 +18,19 @@ func run() {
 		panic(err)
 	}
 
-	win.Clear(colornames.Darkmagenta)
+	imd := imdraw.New(nil)
+
+	imd.Color = pixel.RGB(1, 0, 0)
+	imd.Push(pixel.V(200, 100))
+	imd.Color = pixel.RGB(0, 1, 0)
+	imd.Push(pixel.V(800, 100))
+	imd.Color = pixel.RGB(0, 0, 1)
+	imd.Push(pixel.V(500, 700))
+	imd.Polygon(0)
 
 	for !win.Closed() {
+		win.Clear(colornames.Black)
+		imd.Draw(win)
 		win.Update()
 	}
 }
