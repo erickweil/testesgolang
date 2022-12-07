4ecce3ddabd524666ef957b1ae34764d829cb2ce
Erick Weil <erick.weil@ifro.edu.br>
2022-12-07 20:46:40 -0300

    Implementado add

--- estruturas/listaencadeada/listaencadeada.go
+++ estruturas/listaencadeada/listaencadeada.go
@@ -3,20 +3,41 @@ package listaencadeada
 import "fmt"
 
 type No struct {
-	valor int
+	valor   int
 	proximo *No
 }
 
 type ListaEncadeada struct {
 	inicio *No
-	fim *No
+	fim    *No
 }
 
-func (lista ListaEncadeada) Add(valor int) {
-	fmt.Println("Add",valor)
+func (lista *ListaEncadeada) Add(v int) {
+	var novoNo = No{valor: v, proximo: nil}
+	if lista.fim == nil {
+		lista.inicio = &novoNo
+		lista.fim = &novoNo
+	} else {
+		lista.fim.proximo = &novoNo
+		lista.fim = &novoNo
+	}
+}
+
+func (lista *ListaEncadeada) getno(indice int) *No {
+	return nil
+}
+
+
+func (lista *ListaEncadeada) Get(indice int) int {
+	return lista.getno(indice).valor
 }
 
 func Main() {
 	var lista = ListaEncadeada{}
 	lista.Add(10)
+	lista.Add(20)
+
+	fmt.Println(lista)
+	fmt.Println("Inicio:",lista.inicio)
+	fmt.Println("Fim:",lista.fim)
 }
